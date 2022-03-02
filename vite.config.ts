864167ece334a79473942a6231ce45db090f49de
songchangyun <songchangyun@idea-group.cn>
2022-03-02 17:04:39 +0800

    ✨新增: 初始化项目规范
    代码格式和git commit信息规范

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,13 @@ import Tov from './presets/tov'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	resolve: {
-		alias: {
-			'~/': `${resolve(__dirname, 'src')}/`
-		}
-	},
-	plugins: [Tov()]
+  resolve: {
+    alias: {
+      '~/': `${resolve(__dirname, 'src')}/`
+    }
+  },
+  plugins: [Tov()],
+  server: {
+    host: '0.0.0.0'
+  }
 })
